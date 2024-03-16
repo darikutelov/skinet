84d0d8765d0b2f578ba31748045f3aeb57593c4e
darikutelov <dari@kutelov.com>
2024-03-16 11:36:17 +0200

    Generic Repository

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,43 +3,50 @@ using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Core;
+using Core.Interfaces;
 namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductsController: ControllerBase
+public class ProductsController : ControllerBase
 {
-    private readonly IProductRepository _repository;
+    private readonly IGenericRepository<Product> _productRepository;
+    private readonly IGenericRepository<ProductBrand> _brandRepository;
+    private readonly IGenericRepository<ProductType> _typeRepository;
 
-    public ProductsController(IProductRepository repository)
+    public ProductsController(IGenericRepository<Product> productRepository,
+        IGenericRepository<ProductBrand> brandRepository,
+        IGenericRepository<ProductType> typeRepository
+    )
     {
-        _repository = repository;
+        _productRepository = productRepository;
+        _brandRepository = brandRepository;
+        _typeRepository = typeRepository;
     }
 
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts()
     {
-        var products = await _repository.GetProductsAsync();
+        var products = await _productRepository.GetAllAsync();
 
         return Ok(products);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
-    { 
-        return await _repository.GetProductByIdAsync(id);
+    {
+        return await _productRepository.GetByIdAsync(id);
     }
 
     [HttpGet("brands")]
     public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
     {
-        return Ok(await _repository.GetProductBrandsAsync());
+        return Ok(await _brandRepository.GetAllAsync());
     }
 
-    
     [HttpGet("types")]
     public async Task<ActionResult<List<ProductType>>> GetProductTypes()
     {
-        return Ok(await _repository.GetProductTypesAsync());
+        return Ok(await _typeRepository.GetAllAsync());
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Core;
+using Core.Interfaces;
 using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -11,11 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(x => {
+builder.Services.AddDbContext<StoreContext>(x =>
+{
     x.UseSqlite(builder.Configuration
         .GetConnectionString("DefaultConnection"));
-    });
+});
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+//builder.Services.AddAutoMapper(typeof(MappingProfiles));
 
 var app = builder.Build();
 
@@ -35,7 +39,7 @@ var services = scope.ServiceProvider;
 var context = services.GetRequiredService<StoreContext>();
 var logger = services.GetRequiredService<ILogger<Program>>();
 try
-{ 
+{
     await context.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context);
 }
